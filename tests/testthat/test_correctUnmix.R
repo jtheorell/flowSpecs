0e18811835451036b62703093893242ad4a1e7e7
Jakob Theorell <jakob.theorell@ki.se>
2020-09-03 14:33:32 +0200

    Minor bug changes to avoid check errors.

--- tests/testthat/test_correctUnmix.R
+++ tests/testthat/test_correctUnmix.R
@@ -1,13 +1,14 @@
 # correctUnmix
 context("correctUnmix")
 valueFrame <-
-    flowCore:::flowFrame(matrix(c(1,1, 1, 1), 2, 2,
+    flowCore::flowFrame(matrix(c(1,1, 1, 1), 2, 2,
                      dimnames = list(c(NA, NA), c("V1", "V2"))))
 corrMat <- matrix(c(1, -0.5, 0, 1), nrow = 2, ncol = 2,
                   dimnames = list(c("V1", "V2"), c("V1", "V2")))
+locRes <- as.numeric(exprs(correctUnmix(
+    valueFrame, corrMat, transCoFacs = FALSE))[1,1])
 test_that("correctUnmixResult", {
-    expect_equal(exprs(correctUnmix(
-        valueFrame, corrMat, transCoFacs = FALSE))[1,1], 1.5)
+    expect_equal(locRes, 1.5)
 })
 
 
--- tests/testthat/test_peakNorm.R
+++ tests/testthat/test_peakNorm.R
@@ -3,11 +3,10 @@ extract <- fullPanel[,c(2,20)]
 
 extractPlus1000 <- flowFrame(exprs(extract)+1000)
 
-#Now normalize the new one to the old. NB! Here we will only
+#Now normalize the new one to the old.
 normPanel1000 <- peakNorm(flowSet(extractPlus1000), 1, extract)
 
 #And now check the new result
-
 
 test_that("massOrFlowTransResult", {
     expect_equal(range(exprs(extract)), range(exprs(normPanel1000[[1]])))
